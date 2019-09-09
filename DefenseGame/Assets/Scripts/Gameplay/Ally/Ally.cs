d0e2d37723936e8462f00058a7d84edd8ee1fbb4
frankvega2010 <franco_vega@msn.com>
2019-09-08 21:53:23 -0300

    Improved Ally and Enemies AI.

--- DefenseGame/Assets/Scripts/Gameplay/Ally/Ally.cs
+++ DefenseGame/Assets/Scripts/Gameplay/Ally/Ally.cs
@@ -4,76 +4,161 @@ using UnityEngine;
 
 public class Ally : MonoBehaviour
 {
-    private Rigidbody rig;
-    public Vector3 direction;
+    public enum rotations
+    {
+        left,
+        right,
+        front,
+        back,
+        allDirs
+    }
+
+    public enum states
+    {
+        idle,
+        escape,
+        allStates
+    }
+
+    [Header("Configurable Settings")]
+    public states defaultState;
+    public float speed;
     public float minTime;
     public float maxTime;
-    public float randomTime;
+    public float waitingTime;
 
-    private float timer;
-    private float slerpTimer;
+    [Header("State Settings")]
+    public states currentState;
+    public bool isBeingChased;
+
+    private Rigidbody rig;
+    private Transform target;
+    private TorqueLookRotation lookRotation;
+    private float randomTime;
+    public float timer;
+    public float waitingTimer;
+    public float slerpTimer;
     private float slerpMaxTime;
-    private Vector3 oldDirection;
-    private Vector3 newDirection;
+    private Vector3 direction;
+    private Quaternion rotation;
+    private Quaternion oldRotation;
+    private Quaternion newRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody>();
-        randomDirection();
+        lookRotation = GetComponent<TorqueLookRotation>();
+        randomRotation();
         newRandomTime();
-       // direction = new Vector3(-1, 0, 0);
+        currentState = defaultState;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        slerpTimer += Time.deltaTime;
-
-        if(timer >= randomTime)
+        switch (currentState)
         {
-            randomDirection();
-            newRandomTime();
-            timer = 0;
+            case states.idle:
+                IdleState();
+                break;
+            case states.escape:
+                EscapeState();
+                break;
+            default:
+                break;
         }
-
-        if(slerpTimer <= 1)
-        {
-            newDirection = Vector3.Slerp(oldDirection, direction, slerpTimer);
-        }
-        
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + transform.TransformDirection(newDirection) * 5 * Time.deltaTime); ;
+        rig.MovePosition(rig.position + (direction) * Time.deltaTime);
     }
 
-    private void randomDirection()
+    private void switchState(states newState)
     {
-        oldDirection = direction;
-        int randomNumber = Random.Range(0, 4);       
+        currentState = newState;
 
-        switch (randomNumber)
+        switch (currentState)
         {
-            case 0:
-                direction = new Vector3(-4, 0, 0);
+            case states.idle:
+                lookRotation.enabled = false;
                 break;
-            case 1:
-                direction = new Vector3(4, 0, 0);
+            case states.escape:
+                lookRotation.enabled = true;
+                lookRotation.target = target;
                 break;
-            case 2:
-                direction = new Vector3(0, 0, 4);
+            default:
                 break;
-            case 3:
-                direction = new Vector3(0, 0, -4);
+        }
+    }
+
+    private void IdleState()
+    {
+        if (waitingTimer < waitingTime)
+        {
+            direction = transform.forward * speed * 3.0f;
+            waitingTimer += Time.deltaTime;
+        }
+        else if (waitingTimer >= waitingTime)
+        {
+            direction = transform.forward * speed;
+            timer += Time.deltaTime;
+            slerpTimer += Time.deltaTime;
+
+            if (timer >= randomTime)
+            {
+                randomRotation();
+                newRandomTime();
+                timer = 0;
+            }
+
+            if (slerpTimer <= 1)
+            {
+                newRotation = Quaternion.Slerp(transform.rotation, rotation, slerpTimer);
+                transform.rotation = newRotation;
+            }
+        }
+    }
+
+    private void EscapeState()
+    {
+        direction = transform.forward * speed * 3.0f;
+
+        if(!lookRotation.target)
+        {
+            waitingTimer = 0;
+            timer = 0;
+            slerpTimer = 0;
+            randomRotation();
+            switchState(states.idle);
+        }
+    }
+
+    private void randomRotation()
+    {
+        oldRotation = rotation;
+        rotations randomRotation = (rotations)Random.Range((int)rotations.left, (int)rotations.allDirs);       
+
+        switch (randomRotation)
+        {
+            case rotations.left:
+                rotation = transform.rotation * Quaternion.Euler(new Vector3(0, -90, 0));
+                break;
+            case rotations.right:
+                rotation = transform.rotation * Quaternion.Euler(new Vector3(0, 90, 0));
+                break;
+            case rotations.front:
+                //nothing, keeps going forward.
+                break;
+            case rotations.back:
+                rotation = transform.rotation * Quaternion.Euler(new Vector3(0, 180, 0));
                 break;
             default:
                 break;
         }
 
-        if(oldDirection != direction)
+        if(oldRotation != rotation)
         {
             slerpTimer = 0;
         }
@@ -82,5 +167,28 @@ public class Ally : MonoBehaviour
     private void newRandomTime()
     {
         randomTime = Random.Range(minTime, maxTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "enemyRadius")
+        {
+            target = other.gameObject.transform.parent.transform;
+            switchState(states.escape);
+            timer = 0;
+            slerpTimer = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "enemyRadius")
+        {
+            waitingTimer = 0;
+            timer = 0;
+            slerpTimer = 0;
+            randomRotation();
+            switchState(states.idle);
+        }
     }
 }
--- DefenseGame/Assets/Scripts/Gameplay/Enemy/TorqueLookRotation.cs
+++ DefenseGame/Assets/Scripts/Gameplay/Enemy/TorqueLookRotation.cs
@@ -3,30 +3,55 @@ using System.Collections;
 
 public class TorqueLookRotation : MonoBehaviour
 {
+    public enum directions
+    {
+        forward,
+        backwards,
+        allDirs
+    }
 
+    public directions direction;
     public Transform target;
     public float force = 0.1f;
 
     private Rigidbody rig;
+    private Vector3 currentDirection;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        switch (direction)
+        {
+            case directions.forward:
+                currentDirection = transform.forward;
+                break;
+            case directions.backwards:
+                currentDirection = transform.forward*-1;
+                break;
+            default:
+                break;
+        }
+    }
+
     void FixedUpdate()
     {
+        if(target)
+        {
+            Vector3 targetDelta = target.position - transform.position;
 
-        Vector3 targetDelta = target.position - transform.position;
+            //get the angle between transform.forward and target delta
+            float angleDiff = Vector3.Angle(currentDirection, targetDelta);
 
-        //get the angle between transform.forward and target delta
-        float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+            // get its cross product, which is the axis of rotation to
+            // get from one vector to the other
+            Vector3 cross = Vector3.Cross(currentDirection, targetDelta);
 
-        // get its cross product, which is the axis of rotation to
-        // get from one vector to the other
-        Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
-
-        // apply torque along that axis according to the magnitude of the angle.
-        rig.AddTorque(cross * angleDiff * force);
+            // apply torque along that axis according to the magnitude of the angle.
+            rig.AddTorque(cross * angleDiff * force);
+        }
     }
 }
