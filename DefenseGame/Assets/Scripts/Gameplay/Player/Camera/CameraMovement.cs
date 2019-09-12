8885f98dbbaf391aa3585e482fe259e1d4bffdad
frankvega2010 <franco_vega@msn.com>
2019-09-12 11:28:39 -0300

    Fixed planet collision and WIP Camera zoom

--- DefenseGame/Assets/Scripts/Gameplay/Player/Camera/CameraMovement.cs
+++ DefenseGame/Assets/Scripts/Gameplay/Player/Camera/CameraMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Rigidbody rig;
     public Vector3 direction;
     public Vector3 torque;
     public int speed;
     public int torqueSpeed;
+    public float zoomSpeed = 10;
+    public GameObject cameraGO;
+    public Transform lowestZoom;
+    public Transform highestZoom;
+
+    private Rigidbody rig;
+    [Range(0,1)]
+    public float zoomAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +27,11 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Rotation Horizontal") * torqueSpeed * Time.deltaTime;
-        
+        zoomAmount += Input.GetAxis("Mouse ScrollWheel") *-1 * zoomSpeed * Time.deltaTime;
+        zoomAmount = Mathf.Clamp(zoomAmount,0, 1);
+
+        cameraGO.transform.position = Vector3.Slerp(lowestZoom.transform.position, highestZoom.transform.position, zoomAmount);
+
         chooseDirection();
 
         rig.AddTorque(transform.up * h);
