74e8726457e7a5fc354e89caa2ff904696871653
frankvega2010 <franco_vega@msn.com>
2019-09-12 17:44:21 -0300

    Finished Camera Zoom Acceleration

--- DefenseGame/Assets/Scripts/Gameplay/Player/Camera/CameraMovement.cs
+++ DefenseGame/Assets/Scripts/Gameplay/Player/Camera/CameraMovement.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Vector3 direction;
-    public Vector3 torque;
-    public int speed;
-    public int torqueSpeed;
-    public float zoomSpeed = 10;
+    [Header("References")]
     public GameObject cameraGO;
     public Transform lowestZoom;
     public Transform highestZoom;
+
+    [Header("Speed")]
+    public int speed;
+    public int torqueSpeed;
+    public float zoomSpeed = 10;
+
+    [Header("Zoom Acceleration")]   
+    public float accelerationInput = 0;
+    public float acceleration = 0;
+    public float accelerationDecay = 0.0001f;
     [Range(0, 1)]
     public float zoomAmount = 1;
 
-    [Header("Acceleration")]
-    public float accelerationInput = 0;
-    public float accelerationMultiplier = 2f;
-    public float acceleration = 0;
-    public float accelerationDecay = 0.1f;
 
     private Rigidbody rig;
-    
+    private Vector3 direction;
+    private Vector3 torque;
 
     // Start is called before the first frame update
     void Start()
@@ -39,12 +41,27 @@ public class CameraMovement : MonoBehaviour
 
         if(Mathf.Abs(accelerationInput) > 0.01f)
         {
+            acceleration += Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed * Time.deltaTime;            
+        }
 
-        }               
+        if(acceleration > 0)
+        {
+            acceleration -= accelerationDecay * Time.deltaTime;
+            if (acceleration <= 0)
+            {
+                acceleration = 0;
+            }
+        }
+        else if (acceleration < 0)
+        {
+            acceleration += accelerationDecay * Time.deltaTime;
+            if (acceleration >= 0)
+            {
+                acceleration = 0;
+            }
+        }
 
-        zoomAmount += Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed * Time.deltaTime;
-
-        //zoomAmount += 0.001f;
+        zoomAmount += acceleration;
 
         zoomAmount = Mathf.Clamp(zoomAmount,0, 1);
 
