c508dc138c1fe48d29e37b9339c934998d1b0098
frankvega2010 <franco_vega@msn.com>
2019-10-12 23:03:27 -0300

    Added Version script.

---
+++ DefenseGame/Assets/Scripts/Gameplay/Version.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour
+{
+    private Text versionText;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        versionText = GetComponent<Text>();
+        versionText.text = "v" + Application.version;
+    }
+}
