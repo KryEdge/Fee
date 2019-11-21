265348906bb3f815cdb20dec02d5a25d5a216103
frankvega2010 <franco_vega@msn.com>
2019-11-21 12:17:55 -0300

    Disabled upgrades button unless the player has finished a match

---
+++ Fee/Assets/Scripts/UI/Screens/Menu/UIManagerMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerMenu : MonoBehaviour
+{
+    [Header("General Settings")]
+    public Button upgradesButton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        string goToTutorial = PlayerPrefs.GetString("isFirstTimePlaying", "yes"); // pasarlo a int
+
+        if (goToTutorial == "yes")
+        {
+            upgradesButton.interactable = false;
+            upgradesButton.image.color = Color.gray;
+        }
+        else if (goToTutorial == "no")
+        {
+            upgradesButton.interactable = true;
+            upgradesButton.image.color = Color.white;
+        }
+    }
+
+    /*// Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
