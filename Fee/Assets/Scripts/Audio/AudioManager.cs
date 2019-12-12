fd48a5a6d555797ef5c37b504990de6372303bc1
frankvega2010 <franco_vega@msn.com>
2019-12-12 17:21:48 -0300

    Fixed bug on Issue #15 (Needs to be verified by QA)

--- Fee/Assets/Scripts/Audio/AudioManager.cs
+++ Fee/Assets/Scripts/Audio/AudioManager.cs
@@ -2,22 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class AudioManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class VolumeOptions
+    {
+        public float sfxVol;
+        public float musicVol;
+
+        public string Serialize()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        public void Deserialize(string json)
+        {
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+    }
+
     public Slider sfxSlider;
     public Slider musicSlider;
     public float musicVolume;
     public float sfxVolume;
     private int type1 = 1;
+    private VolumeOptions data;
+    private DataManager dm;
 
     void Start()
     {
-        AkSoundEngine.GetRTPCValue("music_volume", gameObject, 0, out musicVolume, ref type1);
-        AkSoundEngine.GetRTPCValue("sfx_volume", gameObject, 0, out sfxVolume, ref type1);
+        data = new VolumeOptions();
+        dm = DataManager.Instance;
+        string deserializedString;
 
-        musicSlider.value = musicVolume;
-        sfxSlider.value = sfxVolume;
+        if(dm.loadData("soundData.txt",out deserializedString))
+        {
+            data.Deserialize(deserializedString);
+
+            musicVolume = data.musicVol;
+            sfxVolume = data.sfxVol;
+        }
+        else
+        {
+            musicVolume = 70;
+            sfxVolume = 75;
+        }
+
+        if(musicSlider)
+        {
+            musicSlider.value = musicVolume;
+            sfxSlider.value = sfxVolume;
+
+            AkSoundEngine.SetRTPCValue("music_volume", musicSlider.value);
+            AkSoundEngine.SetRTPCValue("sfx_volume", sfxSlider.value);
+        }
+        else
+        {
+            AkSoundEngine.SetRTPCValue("music_volume", musicVolume);
+            AkSoundEngine.SetRTPCValue("sfx_volume", sfxVolume);
+        }
+        
+
+        /*musicSlider.value = musicVolume;
+        sfxSlider.value = sfxVolume;*/
+
+        data.sfxVol = sfxVolume;
+        data.musicVol = musicVolume;
+
+        dm.saveData("soundData.txt", data.Serialize());
     }
 
     public void ChangeMusicVolume()
@@ -30,5 +84,13 @@ public class AudioManager : MonoBehaviour
     {
         Debug.Log("Changin sfx volume to :" + sfxSlider.value);
         AkSoundEngine.SetRTPCValue("sfx_volume", sfxSlider.value);
+    }
+
+    private void OnDestroy()
+    {
+        data.sfxVol = sfxSlider.value;
+        data.musicVol = musicSlider.value;
+
+        dm.saveData("soundData.txt", data.Serialize());
     }
 }
---
+++ Fee/Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    public string dataToSave;
+    private string savePath;
+
+    private string fileSaved;
+    private static DataManager instance =  null;
+    public static DataManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+    }
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Che maestro, esto no funca");
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    public bool saveData(string filename,string data)
+    {
+        bool fileExisted = false;
+
+        if (File.Exists(filename))
+        {
+            fileExisted = true;
+        }
+
+        savePath = Application.persistentDataPath;
+        Debug.Log(savePath);
+
+        fileSaved = Path.Combine(savePath,filename);
+
+        FileStream fsw = File.Open(fileSaved,FileMode.Create);
+        StreamWriter sw = new StreamWriter(fsw);
+
+        dataToSave = data;
+
+        sw.Write(dataToSave);
+
+        sw.Flush();
+        sw.Close();
+        return fileExisted;
+    }
+
+    public bool loadData(string filename, out string data)
+    {
+        savePath = Application.persistentDataPath;
+        fileSaved = Path.Combine(savePath, filename);
+
+        if (File.Exists(fileSaved))
+        {
+            FileStream fsr = File.Open(fileSaved,FileMode.Open);
+            StreamReader sr = new StreamReader(fsr);
+
+            data = sr.ReadToEnd();
+
+            sr.Close();
+            return true;
+        }
+        else
+        {
+            data = null;
+            return false;
+        }    
+    }
+}
